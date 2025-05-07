be9237024ad23f292be33b9d13485fd72600bcd7
Haanyu-yan <2051856548@qq.com>
2025-05-07 20:02:47 +0800

    indexPage

--- mcdc_front/src/main.js
+++ mcdc_front/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import ArcoVue from '@arco-design/web-vue';
+import App from './App.vue';
+import '@arco-design/web-vue/dist/arco.css';
+import { router } from './router'; // 导入路由配置
+import axios from "./axios";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.config.globalProperties.$axios = axios//全局引入axios，所有组件均可使用axios
+app.use(router); // 使用路由中间件
+app.use(ArcoVue);
+app.mount('#app'); // 挂载 Vue 应用到 DOM 元素中
--- mcdc_front/src/views/indexPage.vue
+++ mcdc_front/src/views/indexPage.vue
@@ -1,11 +1,93 @@
-<script setup>
-
-</script>
-
 <template>
-hello
+  <a-layout class="layout-demo" style="height: 100vh">
+    <a-layout>
+      <a-layout-header>
+        <a-menu
+            :openKeys="['1']"
+            :selectedKeys="['0_1']"
+            mode='horizontal'
+        >
+          <a-menu-item key="0_1">
+            <IconHome />
+            Menu 1
+          </a-menu-item>
+          <a-menu-item key="0_2">
+            <IconCalendar />
+            Menu 2
+          </a-menu-item>
+        </a-menu>
+      </a-layout-header>
+      <a-layout style="padding: 20px 20px">
+        <a-layout-content>Content</a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
+<script>
+import { defineComponent, ref } from 'vue';
+import { Message } from '@arco-design/web-vue';
+import {
+  IconHome,
+  IconCalendar,
+} from '@arco-design/web-vue/es/icon';
 
+export default defineComponent({
+  components: {
+    IconHome,
+    IconCalendar,
+  },
+  setup() {
+    const collapsed = ref(false);
+    const onCollapse = (val, type) => {
+      const content = type === 'responsive' ? '触发响应式收缩' : '点击触发收缩';
+      Message.info({
+        content,
+        duration: 2000,
+      });
+      collapsed.value = val;
+    }
+    return {
+      collapsed,
+      onCollapse,
+      onClickMenuItem(key) {
+        Message.info({ content: `You select ${key}`, showIcon: true });
+      }
+    };
+  }
+});
+</script>
 <style scoped>
-
+.layout-demo {
+  height: 500px;
+  background: var(--color-fill-2);
+  border: 1px solid var(--color-border);
+}
+.layout-demo :deep(.arco-layout-sider) .logo {
+  height: 32px;
+  margin: 12px 8px;
+  background: rgba(255, 255, 255, 0.2);
+}
+.layout-demo :deep(.arco-layout-sider-light) .logo{
+  background: var(--color-fill-2);
+}
+.layout-demo :deep(.arco-layout-header)  {
+  height: 50px;
+  line-height: 64px;
+  background: var(--color-bg-3);
+}
+.layout-demo :deep(.arco-layout-content) {
+  color: var(--color-text-2);
+  font-weight: 400;
+  font-size: 14px;
+  background: var(--color-bg-3);
+}
+.layout-demo :deep(.arco-layout-content)  {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: var(--color-white);
+  font-size: 16px;
+  font-stretch: condensed;
+  text-align: center;
+}
 </style>

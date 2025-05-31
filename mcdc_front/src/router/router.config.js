1d2fd13bef164a1e31fcc892ee3af2b00a8b5ea4
Haanyu-yan <2051856548@qq.com>
2025-05-31 16:40:12 +0800

    front

--- mcdc_front/src/router/router.config.js
+++ mcdc_front/src/router/router.config.js
@@ -1,8 +1,21 @@
 export default [
     {
+        path: '/userManagement',// 路由地址
+        name: 'userManagement',
+        component: () => import('../views/userManagement.vue'),// 文件地址
+        children: []
+    },
+    {
         path: '/',// 路由地址
-        name: 'index',
+        name: 'LoginPage',
+        component: () => import('../views/LoginPage.vue'),// 文件地址
+        children: []
+    },
+    {
+        path: '/indexPage',// 路由地址
+        name: 'indexPage',
         component: () => import('../views/indexPage.vue'),// 文件地址
         children: []
     },
+
 ]
--- mcdc_front/src/views/indexPage.vue
+++ mcdc_front/src/views/indexPage.vue
@@ -1,93 +1,152 @@
 <template>
-  <a-layout class="layout-demo" style="height: 100vh">
+  <div class="layout-demo">
     <a-layout>
       <a-layout-header>
-        <a-menu
-            :openKeys="['1']"
-            :selectedKeys="['0_1']"
-            mode='horizontal'
-        >
-          <a-menu-item key="0_1">
-            <IconHome />
-            Menu 1
-          </a-menu-item>
-          <a-menu-item key="0_2">
-            <IconCalendar />
-            Menu 2
-          </a-menu-item>
-        </a-menu>
+        <a-space style="margin:1.5vh 0 0 2vw">
+          <a-button type="primary" style="margin-right: 10px">
+            <template #icon>
+              <icon-plus />
+            </template>
+          </a-button>
+
+          <a-button type="primary" status="success">运行</a-button>
+        </a-space>
       </a-layout-header>
-      <a-layout style="padding: 20px 20px">
+
+      <a-layout>
+<!--        坐滑块-->
+        <a-layout-sider :resize-directions="['right']">
+          <a-tree
+              :default-selected-keys="['0-0-1']"
+              :data="treeData"
+              :show-line="showLine"
+          />
+        </a-layout-sider>
+
+
+
         <a-layout-content>Content</a-layout-content>
+
+
+<!--        右滑块-->
+        <a-layout-sider :resize-directions="['left']">
+          Sider
+        </a-layout-sider>
       </a-layout>
     </a-layout>
-  </a-layout>
+  </div>
 </template>
-<script>
-import { defineComponent, ref } from 'vue';
-import { Message } from '@arco-design/web-vue';
-import {
-  IconHome,
-  IconCalendar,
-} from '@arco-design/web-vue/es/icon';
 
-export default defineComponent({
+<script>
+import {IconPlus} from '@arco-design/web-vue/es/icon';
+import { ref } from 'vue';
+export default ({
   components: {
-    IconHome,
-    IconCalendar,
+    IconPlus,
   },
-  setup() {
-    const collapsed = ref(false);
-    const onCollapse = (val, type) => {
-      const content = type === 'responsive' ? '触发响应式收缩' : '点击触发收缩';
-      Message.info({
-        content,
-        duration: 2000,
-      });
-      collapsed.value = val;
-    }
+  methods: {
+
+  },
+  setup(){
+    const showLine = ref(true);
     return {
-      collapsed,
-      onCollapse,
-      onClickMenuItem(key) {
-        Message.info({ content: `You select ${key}`, showIcon: true });
-      }
+      showLine,
+      treeData,
     };
   }
 });
+
+const treeData = [
+  {
+    title: 'Trunk 1',
+    key: '0-0',
+    children: [
+      {
+        title: 'Trunk 1-000000000',
+        key: '0-0-0',
+        children: [
+          { title: 'leaf000000', key: '0-0-0-0' },
+          {
+            title: 'leaf',
+            key: '0-0-0-1',
+            children: [{ title: 'leaf', key: '0-0-0-1-0' }],
+          },
+          { title: 'leaf', key: '0-0-0-2' },
+        ],
+      },
+      {
+        title: 'Trunk 1-1',
+        key: '0-0-1',
+      },
+      {
+        title: 'Trunk 1-2',
+        key: '0-0-2',
+        children: [
+          { title: 'leaf', key: '0-0-2-0' },
+          {
+            title: 'leaf',
+            key: '0-0-2-1',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Trunk 2',
+    key: '0-1',
+  },
+  {
+    title: 'Trunk 3',
+    key: '0-2',
+    children: [
+      {
+        title: 'Trunk 3-0',
+        key: '0-2-0',
+        children: [
+          { title: 'leaf', key: '0-2-0-0' },
+          { title: 'leaf', key: '0-2-0-1' },
+        ],
+      },
+    ],
+  },
+];
 </script>
+
 <style scoped>
-.layout-demo {
-  height: 500px;
-  background: var(--color-fill-2);
-  border: 1px solid var(--color-border);
-}
-.layout-demo :deep(.arco-layout-sider) .logo {
-  height: 32px;
-  margin: 12px 8px;
-  background: rgba(255, 255, 255, 0.2);
-}
-.layout-demo :deep(.arco-layout-sider-light) .logo{
-  background: var(--color-fill-2);
-}
-.layout-demo :deep(.arco-layout-header)  {
-  height: 50px;
-  line-height: 64px;
-  background: var(--color-bg-3);
-}
+.layout-demo :deep(.arco-layout-header),
+.layout-demo :deep(.arco-layout-sider-children),
 .layout-demo :deep(.arco-layout-content) {
-  color: var(--color-text-2);
-  font-weight: 400;
-  font-size: 14px;
-  background: var(--color-bg-3);
-}
-.layout-demo :deep(.arco-layout-content)  {
   display: flex;
   flex-direction: column;
-  justify-content: center;
   color: var(--color-white);
-  font-size: 16px;
+  font-size: 20px;
   font-stretch: condensed;
   text-align: center;
+}
+
+.layout-demo :deep(.arco-layout-sider-children) {
+  justify-content: flex-start; /* 改为顶部对齐 */
+  margin: 10px;
+}
+
+.layout-demo :deep(.arco-tree) {
+  font-family: 'Microsoft YaHei', sans-serif; /* 设置 a-tree 字体为微软雅黑 */
+}
+
+.layout-demo :deep(.arco-layout-header){
+  height: 7vh;
+  background-color: rgba(1, 25, 85, 0.8);
+}
+
+.layout-demo :deep(.arco-layout-sider) {
+  width: 206px;
+  min-width: 150px;
+  max-width: 30vw;
+  height: 93vh;
+  background-color: rgba(246, 246, 246, 0.8);
+}
+
+.layout-demo :deep(.arco-layout-content) {
+  background-color: rgb(var(--arcoblue-6));
 }
 </style>
